483afe66414a21e4f0fa15b4201034c9ffb25a0f
arie0703 <[ari0703sophia@gmail.com]>
2021-11-05 13:40:41 +0900

    firestoreのデータを表示させる

---
+++ src/components/Favorites.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="favorites">
+        <p class="py-4">My Favorite</p>
+        <div v-for="(favorite, index) in favorites" v-bind:key="index" class="m-auto mt-2 max-w-md py-4 px-8 bg-white shadow-lg rounded-lg">
+            <div class="text-left">
+                <p class="text-2xl">{{favorite.food_name}}</p>
+                <div class="nutrition-info flex">
+                    <div class="nutrition-params">
+                        <li>calories: {{ favorite.calories }}</li>
+                        <li>protein: {{ favorite.protein }}</li>
+                        <li>carbohydrate: {{ favorite.carbohydrate }}</li>
+                        <li>fat: {{ favorite.fat }}</li>
+                    </div>
+
+                    <div class="ml-auto">
+                        <img :src="favorite.image_url">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import {db} from '../main.js';
+
+export default {
+  name: "Favorites",
+  data () {
+    return {
+      favorites: [],
+    }
+  },
+  mounted: function() {
+    this.fetchFavorites();
+  },
+  methods: {
+      fetchFavorites: function() {
+        db.collection("favorites").get()
+        .then((querySnapshot) => {
+            this.favorites = []
+            querySnapshot.forEach((doc) => {
+                this.favorites.push(doc.data())
+            });
+        });
+        console.log(this.favorites)
+      }
+  }  
+}
+</script>
+
+<style scoped>
+
+</style>
