652453f2c30041f83a2422dbfb9f29f376bfd52f
arie0703 <[ari0703sophia@gmail.com]>
2021-11-11 20:44:50 +0900

    検索したアイテムをcalculatorに追加できないのを修正

--- src/components/CalcButton.vue
+++ src/components/CalcButton.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <button v-on:click="calculate($parent.calcData)" class="mt-1 px-3 py-2 flex items-center justify-center rounded-full bg-yellow-500 hover:bg-yellow-600 text-white" type="submit">
+        <button v-on:click="calculate(calcData)" class="mt-1 px-3 py-2 flex items-center justify-center rounded-full bg-yellow-500 hover:bg-yellow-600 text-white" type="submit">
             +
         </button>
     </div>
@@ -15,9 +15,11 @@ export default {
         protein: Number,
         fat: Number,
         carbohydrate: Number,
+        calcData: Object,
     },
     methods: {
         calculate: function(data) {
+            console.log(data)
             data.calories += Math.round(this.calories)
             data.protein += Math.round(this.protein)
             data.fat += Math.round(this.fat)
